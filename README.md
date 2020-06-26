68bd9736659950f6fb1024616c992a92386567ff
mahmod <mahmoudelattar800@gmail.com>
2020-06-26 19:16:31 +0200

    Intial Commit

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# PixelArtMaker
+# Pixel Art Maker Project
+
+## Table of Contents
+
+* [Instructions](#instructions)
+* [Contributing](#contributing)
+
+## Instructions
+
+To get started, open `index.html` and start running out the app,
+you will assign values to the Width and the height, choose the color,
+press submit to see your pixels and press on them to color them.
+
+
+## Contributing
+
+This repository is the starter code for _all_ who want a help.
---
+++ designs.js
@@ -0,0 +1,65 @@
+var width,height,color;
+
+$("#sizePicker").submit(function (e) {
+    e.preventDefault();
+    height = $("#inputHeight").val();
+    width = $("#inputWidth").val();
+    makeGrid(height,width);
+});
+
+
+function makeGrid(x,y) {
+    $('tr').remove();
+    
+    for(var i = 1 ; i<= x ; i++) {
+        
+        //*******************************************//
+        $('#pixelCanvas').append('<tr id=table' + i + '></tr>');
+            //var tr = document.createElement('tr');
+            //tr.setAttribute('id','table'+i);
+            //document.querySelector('#pixelCanvas').append(tr);
+        //*******************************************//
+        
+         for(var j = 1 ; j<= y ; j++) {
+                $('#table' + i).append('<td></td>');
+         }
+    }
+    
+    $('td').click(function() {
+        color = $("#colorPicker").val();
+        
+        if($(this).attr('style')) {
+            $(this).removeAttr('style');
+        }else {
+                $(this).attr('style','background-color:'+ color);
+        }    
+    });
+};
+
+
+document.querySelector('#NL').addEventListener('click', function() {
+        $('tr').remove();
+    document.querySelector('#NL').style.backgroundColor = "blue";
+    document.querySelector('#EN').style.backgroundColor = "rgba(0,0,0,0)";
+    document.querySelector('p').innerHTML = "Voli<span>Mahmoud Elattar</span>";
+    document.querySelector('#ch').textContent = "Kies rastergrootte";
+    document.querySelector('#H').textContent = "Rooster hoogte:";
+    document.querySelector('#W').textContent = "Raster breedte:";
+    document.querySelector('#pick').textContent = "Kies een kleur";
+    document.querySelector('#Design').textContent = "Ontwerp canvas";
+    document.querySelector('#submit').value = "voorleggen";
+    
+});
+document.querySelector('#EN').addEventListener('click', function() {
+        $('tr').remove();
+    document.querySelector('#EN').style.backgroundColor = "blue";
+    document.querySelector('#NL').style.backgroundColor = "rgba(0,0,0,0)";
+    document.querySelector('p').innerHTML = "Created By<span>Mahmoud Elattar</span>";
+    document.querySelector('h1').textContent = "Pixel Art Maker";
+    document.querySelector('#ch').textContent = "Choose Grid Size";
+    document.querySelector('#H').textContent = "Grid Height:";
+    document.querySelector('#W').textContent = "Grid Width:";
+    document.querySelector('#pick').textContent = "Pick A Color";
+    document.querySelector('#Design').textContent = "Design Canvas";
+    document.querySelector('#submit').value = "submit";
+});
